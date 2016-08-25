a848bc534372fe35b5397a5784f042883cecf551
Emmanuel Obispo <e.obispo@hotmail.com>
2016-08-24 21:48:24 -0500

    Create Ejercicio-cubo.js

---
+++ rv/Ejercicio-cubo.js
@@ -0,0 +1,42 @@
+var forma = new THREE.Geometry();
+
+forma.vertices.push( new THREE.Vector3( 1, -1 , 1 ) );   // Push empuja elementos al arreglo, este es el elemento 0
+forma.vertices.push( new THREE.Vector3( 1, -1 , -1 ) );   // Elemento 1
+forma.vertices.push( new THREE.Vector3( -1, -1 , -1 ) );
+forma.vertices.push( new THREE.Vector3( -1, -1 , 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, 1 , 1 ) );
+forma.vertices.push( new THREE.Vector3( 1, 1 , -1 ) );
+forma.vertices.push( new THREE.Vector3( -1, 1 , -1 ) );
+forma.vertices.push( new THREE.Vector3( -1, 1 , 1 ) );   // Elemento 7
+
+forma.faces.push( new THREE.Face3( 3, 2, 1 ) );   // Cara 0
+forma.faces.push( new THREE.Face3( 3, 1, 0 ) );   // Cara 1
+forma.faces.push( new THREE.Face3( 5, 1, 0 ) );   // Cara 2
+forma.faces.push( new THREE.Face3( 5, 0, 4 ) );   // Cara 3
+forma.faces.push( new THREE.Face3( 5, 1, 2 ) );   // Cara 4
+forma.faces.push( new THREE.Face3( 5, 2, 6 ) );   // Cara 5
+forma.faces.push( new THREE.Face3( 5, 6, 7 ) );   // Cara 3
+forma.faces.push( new THREE.Face3( 5, 7, 4 ) );   // Cara 4
+forma.faces.push( new THREE.Face3( 7, 3, 2 ) );   // Cara 5
+forma.faces.push( new THREE.Face3( 7, 2, 6 ) );   // Cara 3
+forma.faces.push( new THREE.Face3( 7, 4, 0 ) );   // Cara 4
+forma.faces.push( new THREE.Face3( 7, 0, 3 ) );   // Cara 5
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
