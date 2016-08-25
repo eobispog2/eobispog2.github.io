ea79670936382df45031a90cdb35a55f880550ac
Emmanuel Obispo <e.obispo@hotmail.com>
2016-08-24 23:03:20 -0500

    Update Ejercicio-cubo.js

--- rv/Ejercicio-cubo.js
+++ rv/Ejercicio-cubo.js
@@ -9,8 +9,7 @@ forma.vertices.push( new THREE.Vector3( 1, -1 , 1 ) );
 forma.vertices.push( new THREE.Vector3( 1, 1 , 1 ) );
 forma.vertices.push( new THREE.Vector3( -1, 1 , 1 ) );   // Elemento 7
 
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) );   // Cara 0
-forma.faces.push( new THREE.Face3( 1, 3, 0 ) );   // Cara 1
+
 forma.faces.push( new THREE.Face3( 4, 5, 6 ) );   // Cara 2
 forma.faces.push( new THREE.Face3( 4, 6, 7 ) );   // Cara 3
 
