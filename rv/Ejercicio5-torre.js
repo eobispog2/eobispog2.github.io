c27a65a5b48de6850834eaf5e6eca7f58fd7d08b
Emmanuel Obispo <e.obispo@hotmail.com>
2016-08-31 23:28:53 -0500

    Update Ejercicio5-torre.js

--- rv/Ejercicio5-torre.js
+++ rv/Ejercicio5-torre.js
@@ -11,24 +11,24 @@ puntos.push( new THREE.Vector2( 0, 12 ) );
 var torreForma1 = new THREE.LatheGeometry(puntos);
 var torreMalla1 = new THREE.Mesh(torreForma1);
 
-var torreForma2 = new THREE.BoxGeometry( 8, 2, 8 );
+var torreForma2 = new THREE.BoxGeometry( 7, 2, 7 );
 torreForma2.translate(0,12,0);
 var torreMalla2 = new THREE.Mesh(torreForma2);
 
 var torreForma3 = new THREE.BoxGeometry( 2, 2, 2 );
-torreForma3.translate(3,14,3);
+torreForma3.translate(2,14,2);
 var torreMalla3 = new THREE.Mesh(torreForma3);
 
 var torreForma4 = new THREE.BoxGeometry( 2, 2, 2 );
-torreForma4.translate(-3,14,3);
+torreForma4.translate(-2,14,2);
 var torreMalla4 = new THREE.Mesh(torreForma4);
 
 var torreForma5 = new THREE.BoxGeometry( 2, 2, 2 );
-torreForma5.translate(3,14,-3);
+torreForma5.translate(2,14,-2);
 var torreMalla5 = new THREE.Mesh(torreForma5);
 
 var torreForma6 = new THREE.BoxGeometry( 2, 2, 2 );
-torreForma6.translate(-3,14,-3);
+torreForma6.translate(-2,14,-2);
 var torreMalla6 = new THREE.Mesh(torreForma6);
 
 //JUNTAR MALLAS:
