20ab4c144401feb8b8b80f1410483d9ef31fc3dc
Emmanuel Obispo <e.obispo@hotmail.com>
2016-08-31 23:47:24 -0500

    Update Ejercicio5-torre.js

--- rv/Ejercicio5-torre.js
+++ rv/Ejercicio5-torre.js
@@ -50,7 +50,7 @@ escena.add(torreMalla);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 50;
-camara.position.y = 12;
+camara.position.y = 7;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
