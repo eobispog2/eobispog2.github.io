affb3834b462e76f60063efc20c1c2a23e6fdfe9
Emmanuel Obispo <e.obispo@hotmail.com>
2016-08-31 23:42:35 -0500

    Update Ejercicio5-torre.js

--- rv/Ejercicio5-torre.js
+++ rv/Ejercicio5-torre.js
@@ -12,7 +12,7 @@ var torreForma1 = new THREE.LatheGeometry(puntos);
 var torreMalla1 = new THREE.Mesh(torreForma1);
 
 var torreForma2 = new THREE.BoxGeometry( 7.5, 2, 7.5 );
-torreForma2.translate(0,12,0);
+torreForma2.translate(0,13,0);
 var torreMalla2 = new THREE.Mesh(torreForma2);
 
 var torreForma3 = new THREE.BoxGeometry( 2, 1.5, 2 );
