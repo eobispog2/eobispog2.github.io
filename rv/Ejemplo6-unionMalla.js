5b0489efd4aca78283abe58fc94086bbc4cd32d3
Emmanuel Obispo <e.obispo@hotmail.com>
2016-08-31 21:22:28 -0500

    Update Ejemplo6-unionMalla.js

--- rv/Ejemplo6-unionMalla.js
+++ rv/Ejemplo6-unionMalla.js
@@ -17,7 +17,6 @@ escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
-camara.position.y = 5;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
