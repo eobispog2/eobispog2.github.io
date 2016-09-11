627464fd75bf1b290f6a9c235b6e6bad0968c36f
Emmanuel Obispo <e.obispo@hotmail.com>
2016-09-10 19:46:19 -0500

    Update Ejercicio6-tablero.js

--- rv/Ejercicio6-tablero.js
+++ rv/Ejercicio6-tablero.js
@@ -72,9 +72,9 @@ var ambientLight = new THREE.AmbientLight( 0x000000 );
 escena.add( ambientLight );
 
 var lights = [];
-lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
-lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[ 0 ] = new THREE.PointLight( 0xff00ff, 1, 0 );
+lights[ 1 ] = new THREE.PointLight( 0xff00ff, 1, 0 );
+lights[ 2 ] = new THREE.PointLight( 0xff00ff, 1, 0 );
 lights[ 0 ].position.set( 0, 200, 0 );
 lights[ 1 ].position.set( 100, 200, 100 );
 lights[ 2 ].position.set( - 100, - 200, - 100 );
