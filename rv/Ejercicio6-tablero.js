54d603c9a14756192a0ba8570fe6da79a0f4a054
Emmanuel Obispo <e.obispo@hotmail.com>
2016-09-10 21:09:45 -0500

    Update Ejercicio6-tablero.js

--- rv/Ejercicio6-tablero.js
+++ rv/Ejercicio6-tablero.js
@@ -68,7 +68,12 @@ for (var i = 0; i < 8; i++) {
   for (var j = 0; j < 8; j++) {
     cuadros[counter] = new THREE.BoxGeometry( 10, 10, 2 );
     cuadros[counter].translate(j*10,0,i*10);
-    cuadroMallas[counter] = new THREE.Mesh(cuadros[counter],verdeOscuro);
+    if(numero % 2 == 0) {
+      cuadroMallas[counter] = new THREE.Mesh(cuadros[counter],verdeOscuro);
+    }
+    else {
+      cuadroMallas[counter] = new THREE.Mesh(cuadros[counter],verdeClaro);
+    }
     escena.add(cuadroMallas[counter]);
     counter++;
   }
