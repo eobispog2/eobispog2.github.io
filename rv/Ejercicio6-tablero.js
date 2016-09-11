3e529ccfad5a7b0341e4ef9e3c922e3e59b1f78c
Emmanuel Obispo <e.obispo@hotmail.com>
2016-09-10 20:52:45 -0500

    Update Ejercicio6-tablero.js

--- rv/Ejercicio6-tablero.js
+++ rv/Ejercicio6-tablero.js
@@ -55,19 +55,15 @@ torre4.position.z=80;
 // Formar tablero:
 var verdeOscuro = new THREE.MeshBasicMaterial( { color: 0x3C9823 } );
 var verdeClaro = new THREE.MeshBasicMaterial( { color: 0xB1FB9C } );
-var cuadros = []; // Create a new empty array.
+var cuadros = [new THREE.BoxGeometry( 10, 10, 2 );]; // Create a new empty array.
 var counter = 1;
 for (var i = 0; i < 8; i++) {
   for (var j = 0; j < 8; j++) {
-    cuadros[counter] = new THREE.BoxGeometry( 10, 10, 2 );
+    //cuadros[counter] = new THREE.BoxGeometry( 10, 10, 2 );
     cuadros[counter].translate(j*10,i*10,0);
     var cuadroMallas[counter] = new THREE.Mesh(cuadros[counter],verdeOscuro);
     counter++;
   }
-}
-
-for (i = 0; i < cars.length; i++) { 
-    text += cars[i] + "<br>";
 }
 
 var escena = new THREE.Scene();
