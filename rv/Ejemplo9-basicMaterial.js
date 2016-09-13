343dce6b8a0f0f0d69d75b6a888935a22cb0124c
Emmanuel Obispo <e.obispo@hotmail.com>
2016-09-13 14:51:36 -0500

    Update Ejemplo9-basicMaterial.js

--- rv/Ejemplo9-basicMaterial.js
+++ rv/Ejemplo9-basicMaterial.js
@@ -5,7 +5,7 @@ var malla = new THREE.Mesh( forma, material );
 var escena = new THREE.Scene();
 escena.add(malla);
 
-vara camara = new THREE.PerspectiveCamera();
+var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
 var lienzo = document.getElementById("basicMaterial");
