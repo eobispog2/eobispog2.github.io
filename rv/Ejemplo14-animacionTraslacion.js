fcb80c1f693c63204a020710aeddc84d8c3a0faf
Emmanuel Obispo <e.obispo@hotmail.com>
2016-09-22 23:51:43 -0500

    Create Ejemplo14-animacionTraslacion.js

---
+++ rv/Ejemplo14-animacionTraslacion.js
@@ -0,0 +1,25 @@
+function init(p){
+malla = new THREE.Mesh(new THREE.BoxGeometry(p, p, p), new THREE.MeshNormalMaterial());
+escena = new THREE.Scene();
+escena.add(malla);
+camara = new THREE.PerspectiveCamera();
+camara.position.z = 10*p;
+step=0.01;
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(700,700);
+document.body.appendChild(renderizador.domElement);
+}
+
+var loop = function(){
+requestAnimationFrame(loop);
+renderizador.render(escena, camara);
+malla.translateX(step);
+if(Math.abs(malla.position.x)>=2)
+step=-step;
+malla.position.x+=step;
+}
+
+var escena, camara, renderizador, malla, step;
+
+init(1);
+loop();
