a07b99b6e44845890461a63852a187f1ab1c359a
Emmanuel Obispo <e.obispo@hotmail.com>
2016-09-27 15:42:40 -0500

    Update Ejemplo16-textura.js

--- rv/Ejemplo16-textura.js
+++ rv/Ejemplo16-textura.js
@@ -2,7 +2,7 @@ var TEXTURA = new Object();
 
 TEXTURA.retrollamada = function( textura ) {
   var material = new THREE.MeshBasicMaterial( {map: textura} );
-  TEXTURA.malla = neww THREE.Mesh( new THREE.SphereGeometry( 1 ), material );
+  TEXTURA.malla = new THREE.Mesh( new THREE.SphereGeometry( 1 ), material );
   
   TEXTURA.escena.add(TEXTURA.malla);
 }
