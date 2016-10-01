fe20fe742fa73828c895ae4c68a85eb6fc9187de
Emmanuel Obispo <e.obispo@hotmail.com>
2016-10-01 00:57:39 -0500

    Update Ejemplo16-textura.js

--- rv/Ejemplo16-textura.js
+++ rv/Ejemplo16-textura.js
@@ -24,6 +24,10 @@ TEXTURA.setup = function() {
 
 TEXTURA.loop = function() {
   requestAnimationFrame( TEXTURA.loop);
+  if ( TEXTURA.malla !== undefined ) {
+    TEXTURA.malla.rotateX( 0.01);
+    TEXTURA.malla.rotateY( 0.01);
+  }
   TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara );
 }
 
