30fe044bd7badc37371913864eb1c24470e271b2
Emmanuel Obispo <e.obispo@hotmail.com>
2016-10-04 15:08:40 -0500

    Create Ejemplo17-constructor.js

---
+++ rv/Ejemplo17-constructor.js
@@ -0,0 +1,32 @@
+function Arbol(){
+  var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
+  var esferaForma = new THREE.SphereGeometry(.65);
+  esferaForma.translate(0,1,0);
+  
+  var toncoMalla = new THREE.Mesh(troncoForma);
+  var esferaMalla = bew THREE.Mesh(esferaForma);
+  
+  var arbolForma = new THREE.Geometry();
+  arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
+  arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+  
+  var material = new THREE.MeshNormalMaterial();
+  this.malla = new THREE.Mesh(arbolForma, material);
+}
+  
+var CONSTRUCTOR = new Object();
+  
+CONSTRUCTOR.setup = function(){
+  var arbol1 = new Arbol();
+  var arbol2 = new Arbol();
+  
+  arbol1.malla.position.x = -5;
+  arbol2.malla.position.x = 5;
+  
+  CONSTRUCTOR.camara = new THREE.PerspectiveCamera();
+  CONSTRUCTOR.camara.position.z = 20;
+  
+  var lienzo = document.getElementById("Ejemplo17-constructor");
+  CONSTRUCTOR.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  
+  CONSTRUCTOR.renderizador.setSize( 600, 600 );
