76e8d3ccbcc0d760ca55e2b8509461e4747881aa
Emmanuel Obispo <e.obispo@hotmail.com>
2016-10-27 15:52:11 -0500

    Update Ejemplo21-robot.js

--- rv/Ejemplo21-robot.js
+++ rv/Ejemplo21-robot.js
@@ -48,7 +48,7 @@ function Wall(size, x, y) {
 Wall.prototype = new THREE.Mesh();
 
 Environment.prototype.setMap = function(map) {
-  var _offset = Math.floor(map.legth/2);
+  var _offset = Math.floor(map.length/2);
   
   for ( var i = 0; i < map.length; i++)
   for ( var j = 0; j < map.length; j++) {
