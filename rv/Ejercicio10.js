ca45136d4ecfbc91fcf50f2ace5a6c34271ad0e0
Emmanuel Obispo <e.obispo@hotmail.com>
2016-11-06 22:13:48 -0600

    Update Ejercicio10.js

--- rv/Ejercicio10.js
+++ rv/Ejercicio10.js
@@ -379,8 +379,8 @@ CABALLO.CaballoGeometry = function(){
   caballoFigura.moveTo(3.5, 4.5);
   
   var caballoForma2 = new THREE.ExtrudeGeometry( caballoFigura, {amount: 3, bevelEnabled: false} );
+  caballoForma2.translate(0,0,-1.5);
   var caballoMalla2 = new THREE.Mesh(caballoForma2);
-  caballoMalla2.translate(0,0,-3);
 
   // Juntar mallas de la caballo:
   var caballoForma = new THREE.Geometry();
