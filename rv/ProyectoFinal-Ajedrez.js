7109ccb03b5a0bf77df2c00f68889b5c313ddda7
Emmanuel Obispo <e.obispo@hotmail.com>
2016-11-27 02:24:05 -0600

    Update ProyectoFinal-Ajedrez.js

--- rv/ProyectoFinal-Ajedrez.js
+++ rv/ProyectoFinal-Ajedrez.js
@@ -586,9 +586,35 @@ function onDocumentMouseDown( event ) {
   // calculate objects intersecting the picking ray
   var intersects = raycaster.intersectObjects( scene.children );
   
-  if ( intersects.length > 0 ) {
-	  intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
-  }
+  // INTERSECTED = the object in the scene currently closest to the camera 
+	//      and intersected by the Ray projected from the mouse position    
+
+	// if there is one (or more) intersections
+	if ( intersects.length > 0 )
+	{
+			// if the closest object intersected is not the currently stored intersection object
+			if ( intersects[ 0 ].object != INTERSECTED )
+			{
+					// restore previous intersection object (if it exists) to its original color
+					if ( INTERSECTED )
+							INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+					// store reference to closest object as current intersection object
+					INTERSECTED = intersects[ 0 ].object;
+					// store color of closest object (for later restoration)
+					INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+					// set a new color for closest object
+					INTERSECTED.material.color.setHex( 0xffff00 );
+			}
+	}
+	else // there are no intersections
+	{
+			// restore previous intersection object (if it exists) to its original color
+			if ( INTERSECTED )
+					INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+			// remove previous intersection object reference
+			//     by setting current intersection object to "nothing"
+			INTERSECTED = null;
+	}
 }
 
 
@@ -635,6 +661,7 @@ var objects = [];
 var recursiveFlag;
 var raycaster = new THREE.Raycaster(); // create once
 var mouse = new THREE.Vector2(); // create once
+var intersects = raycaster.intersectObjects( scene.children );
 
 
 setup1();
