8a69b42c199bb2a675eefbafd69a420c6e19f01a
Emmanuel Obispo <e.obispo@hotmail.com>
2016-12-03 21:34:53 -0600

    Update Proyecto-Ajedrez.js

--- rv/Proyecto-Ajedrez.js
+++ rv/Proyecto-Ajedrez.js
@@ -738,7 +738,7 @@ function setup(){
   scene.add(caballo3);
   scene.add(caballo4);
   
-  var lienzo = document.getElementById("ProyectoFinal-Ajedrez");
+  var lienzo = document.getElementById("Proyecto-Ajedrez");
   renderer = new THREE.WebGLRenderer({canvas: lienzo, antialias: true})
   renderer.setSize( window.innerWidth*.98, window.innerHeight*.98);
   
