92d88a4c7b77dab2986ec2149060c626a7a90541
Emmanuel Obispo <e.obispo@hotmail.com>
2016-12-04 20:16:35 -0600

    Update Proyecto-TableroAmbiente.js

--- rv/Proyecto-TableroAmbiente.js
+++ rv/Proyecto-TableroAmbiente.js
@@ -32,8 +32,8 @@ CuadroBlanco.prototype= new THREE.Mesh();
 
 Environment.prototype.setMap= function(map){
   var _offset= Math.floor(map.length/2);
-  for(var j=0; j<10; j++)
-  for(var i=0; i<85; i++){
+  for(var j=0; j<11; j++)
+  for(var i=0; i<84; i++){
     if (map[i][j]==="x")
       this.add(new Marco(5,j-_offset,(i-_offset)));
     else if (map[i][j]==="n")
