281eccd7dca93dbd7359a9351022b8fb03a6de51
Emmanuel Obispo <e.obispo@hotmail.com>
2016-12-04 18:55:14 -0600

    Update Proyecto-TableroAmbiente.js

--- rv/Proyecto-TableroAmbiente.js
+++ rv/Proyecto-TableroAmbiente.js
@@ -113,7 +113,6 @@ function setup(){
    // LUCES
   var luz = new THREE.PointLight( 0xffffff, 1, 150, 1.5 );
   luz.position.set(35, 70, 35);
-  scene.add( luz );
   var l1 = new THREE.DirectionalLight( 0xffffff, 0.45 );
   l1.position.set( -10, 25, -10 )
   var l2 = new THREE.DirectionalLight( 0xffffff, 0.45 );
@@ -122,6 +121,8 @@ function setup(){
   l3.position.set( -10, 25, 90 )
   var l4 = new THREE.DirectionalLight( 0xffffff, 0.45 );
   l4.position.set( 90, 25, 90 )
+  
+  environment.add( luz );
   environment.add( l1 );
   environment.add( l2 );
   environment.add( l3 );
