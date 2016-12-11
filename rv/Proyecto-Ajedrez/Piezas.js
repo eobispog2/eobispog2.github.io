55e784224e8c2ff59cc417be88a5298017dd14e1
eobispog2 <eobispog@gmail.com>
2016-12-10 20:54:09 -0600

    Update Piezas.js

--- rv/Proyecto-Ajedrez/Piezas.js
+++ rv/Proyecto-Ajedrez/Piezas.js
@@ -58,7 +58,7 @@ TORRE.TorreGeometry = function(){
 TORRE.TorreGeometry.prototype = new THREE.Geometry();
 
 function Torre_n(x,y){  
-  //Agent.call(this, x, y);  
+  Agent.call(this, x, y);  
   //this.sensor = new Sensor();
   this.actuator = new THREE.Mesh(new TORRE.TorreGeometry(), new THREE.MeshPhongMaterial({map: texture4_g_o}));
   this.position.x=x;
