2f7f58c9460c5944935a2f18b55093ba84d49f86
eobispog2 <eobispog@gmail.com>
2016-12-10 19:09:22 -0600

    Update Proyecto-TableroAmbiente.js

--- rv/Proyecto-Ajedrez/Proyecto-TableroAmbiente.js
+++ rv/Proyecto-Ajedrez/Proyecto-TableroAmbiente.js
@@ -24,63 +24,6 @@ function Cuadro(size,x,y,tex){
 Cuadro.prototype= new THREE.Mesh();
 
 
-//TORRE
-TORRE = new Object();
-
-TORRE.TorreGeometry = function(){
-  THREE.Geometry.call(this);
-  
-  var puntos = [];
-  puntos.push( new THREE.Vector2( 0, 0 ) );
-  puntos.push( new THREE.Vector2( 4.5, 0 ) );
-  puntos.push( new THREE.Vector2( 4.5, 2 ) );
-  puntos.push( new THREE.Vector2( 3.5, 2 ) );
-  puntos.push( new THREE.Vector2( 3.5, 4 ) );
-  puntos.push( new THREE.Vector2( 2.5, 6 ) );
-  puntos.push( new THREE.Vector2( 2.5, 10 ) );
-  puntos.push( new THREE.Vector2( 3.5, 12 ) );
-  puntos.push( new THREE.Vector2( 0, 12 ) );
-  var torreForma1 = new THREE.LatheGeometry(puntos);
-  var torreMalla1 = new THREE.Mesh(torreForma1);
-
-  var torreForma2 = new THREE.BoxGeometry( 7.5, 2, 7.5 );
-  torreForma2.translate(0,13,0);
-  var torreMalla2 = new THREE.Mesh(torreForma2);
-
-  var torreForma3 = new THREE.BoxGeometry( 2, 1.5, 2 );
-  torreForma3.translate(2.75,14.5,2.75);
-  var torreMalla3 = new THREE.Mesh(torreForma3);
-
-  var torreForma4 = new THREE.BoxGeometry( 2, 1.5, 2 );
-  torreForma4.translate(-2.75,14.5,2.75);
-  var torreMalla4 = new THREE.Mesh(torreForma4);
-
-  var torreForma5 = new THREE.BoxGeometry( 2, 1.5, 2 );
-  torreForma5.translate(2.75,14.5,-2.75);
-  var torreMalla5 = new THREE.Mesh(torreForma5);
-
-  var torreForma6 = new THREE.BoxGeometry( 2, 1.5, 2 );
-  torreForma6.translate(-2.75,14.5,-2.75);
-  var torreMalla6 = new THREE.Mesh(torreForma6);
-
-  //JUNTAR MALLAS:
-  var torreForma = new THREE.Geometry();
-  this.merge(torreMalla1.geometry, torreMalla1.matrix);
-  this.merge(torreMalla2.geometry, torreMalla2.matrix);
-  this.merge(torreMalla3.geometry, torreMalla3.matrix);
-  this.merge(torreMalla4.geometry, torreMalla4.matrix);
-  this.merge(torreMalla5.geometry, torreMalla5.matrix);
-  this.merge(torreMalla6.geometry, torreMalla6.matrix);
-}
-TORRE.TorreGeometry.prototype = new THREE.Geometry();
-
-function Torre(x,y,tex){
-  THREE.Mesh.call(this, new TORRE.TorreGeometry(), new THREE.MeshPhongMaterial({map: tex}));	
-  this.position.x=x;
-  this.position.z=y;
-}  
-Torre.prototype = new THREE.Mesh();
-
 //PEON
 PEON = new Object();
 
