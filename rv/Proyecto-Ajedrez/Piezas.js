cf00672836a2a4b473f7299d796190a0a9550799
eobispog2 <eobispog@gmail.com>
2016-12-12 00:12:53 -0600

    Update Piezas.js

--- rv/Proyecto-Ajedrez/Piezas.js
+++ rv/Proyecto-Ajedrez/Piezas.js
@@ -435,7 +435,7 @@ Peon_b1.prototype.sense = function(environment) {
   var obstaculo_i = this.sensor_i.intersectObjects(environment.children, true);
   var obstaculo_d = this.sensor_d.intersectObjects(environment.children, true);
   
-  if ((obstaculo_f.length > 0 && (obstaculo_f[0].distance <= 0.04)))
+  if ((obstaculo_f.length > 0 && (obstaculo_f[0].distance <= 0.5)))
     this.sensor_f.colision = true;
   else
     this.sensor_f.colision = false;
