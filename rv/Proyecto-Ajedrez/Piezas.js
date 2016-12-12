bb6deb91149503af5115f51ac9db067731378f00
eobispog2 <eobispog@gmail.com>
2016-12-11 22:32:27 -0600

    Update Piezas.js

--- rv/Proyecto-Ajedrez/Piezas.js
+++ rv/Proyecto-Ajedrez/Piezas.js
@@ -427,8 +427,8 @@ Caballo_b.prototype = new Agent();
 Peon_b1.prototype.sense = function(environment) {
   this.sensor_f.set(this.position, new THREE.Vector3(Math.sin(this.rotation.y), 0, -Math.cos(this.rotation.y)));
   this.sensor_a.set(this.position, new THREE.Vector3(-Math.sin(this.rotation.y), 0, Math.cos(this.rotation.y)));
-  this.sensor_i.set(this.position, new THREE.Vector3(-Math.cos(this.rotation.y), 0, Math.sin(this.rotation.y)));
-  this.sensor_d.set(this.position, new THREE.Vector3(Math.cos(this.rotation.y), 0, -Math.sin(this.rotation.y)));
+  this.sensor_i.set(this.position, new THREE.Vector3(Math.cos(this.rotation.y), 0, -Math.sin(this.rotation.y)));
+  this.sensor_d.set(this.position, new THREE.Vector3(-Math.cos(this.rotation.y), 0, Math.sin(this.rotation.y)));
   
   var obstaculo_f = this.sensor_f.intersectObjects(environment.children, true);
   var obstaculo_a = this.sensor_a.intersectObjects(environment.children, true);
@@ -443,7 +443,7 @@ Peon_b1.prototype.sense = function(environment) {
 
 Peon_b1.prototype.plan = function(environment) {
   this.actuator.commands = [];
-  if (this.sensor_f.colision == true || this.sensor_i.colision == true)
+  if (this.sensor_f.colision == true)
     this.actuator.commands.push('goRight');
   else
     this.actuator.commands.push('goStraight');
